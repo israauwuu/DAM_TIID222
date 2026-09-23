aac40c82c056b3aa0964038d1d6a6b9052844048
Israel <125054125@upq.edu.mx>
2026-09-22 23:48:03 -0600

    Termine mi modulo individual
    Volvi a trabajar con mi modulo individualmente ya que trabajar en el codigo con los tres modulos integrados se me dificulto bastante.

--- Restaurante/cocina.js
+++ Restaurante/cocina.js
@@ -1,24 +1,37 @@
-const readline = require("readline"); 
-const rl = readline.createInterface({
-    input: process.stdin, 
-    output: process.stdout 
-});
+const readline = require('readline/promises');
+const { stdin: input, stdout: output } = require('process');
 
-let productos = [];
+const rl = readline.createInterface({ input, output }); // esto es para leer desde la consola
+
+let productos = [
+  { nombre: "Café Americano", precio: 15, categoria: "Bebida" },
+  { nombre: "Chocolate Caliente", precio: 18, categoria: "Bebida" },
+  { nombre: "Té", precio: 12, categoria: "Bebida" },
+  { nombre: "Agua de sabor", precio: 10, categoria: "Bebida" },
+  { nombre: "Refresco", precio: 15, categoria: "Bebida" },
+  { nombre: "Pan Dulce", precio: 10, categoria: "Postre" },
+  { nombre: "Dona", precio: 12, categoria: "Postre" },
+  { nombre: "Galletas", precio: 8, categoria: "Postre" },
+  { nombre: "Sandwich", precio: 25, categoria: "Postre" },
+  { nombre: "Torta", precio: 30, categoria: "Postre" },
+  { nombre: "Papas fritas", precio: 15, categoria: "Postre" },
+  { nombre: "Fruta picada", precio: 15, categoria: "Postre" },
+  { nombre: "Yogurt", precio: 15, categoria: "Postre" },
+  { nombre: "Gelatina", precio: 10, categoria: "Postre" }];
 
 //funcion para mostrar el menu
-function menu(){
+async function menu(){
 
     console.log("\n==== SISTEMA DE COCINA =====\n")
     console.log("1. Agregar producto");
     console.log("2. Mostrar productos");
     console.log("3. Editar producto");
-    console.log("4. Eliminar producto");
-    console.log("5. Salir");
+    console.log("4. Buscar producto")
+    console.log("5. Eliminar producto");
+    console.log("6. Salir");
 
-    rl.question("Seleccione una opción: ", function(opcion) {
-
-        switch(opcion) {
+    let opcion = await rl.question("Seleccione una opción: ");
+    switch(opcion) {
 
             case "1":
                 agregarProducto();
@@ -30,26 +43,28 @@ function menu(){
                 editarProducto();
                 break;
             case "4":
-                eliminarProducto();
+                buscarProducto();
                 break;
             case "5":
+                eliminarProducto();
+                break;
+            case "6": 
                 console.log("Saliendo del sistema...");
                 rl.close();
                 break;
             default:
                 console.log("Opción inválida. Intente de nuevo.");
                 menu();
-        }
-    });
+    }
 }
 
 //Funcion para agregrar productos
-function agregarProducto() {
+ async function agregarProducto() {
     
-    rl.question("Ingrese el nombre del producto: ", function(nombre) {
-        rl.question("Ingrese el precio del producto: ",function(precio){
-            
-            let producto = {
+    let nombre = await rl.question("Ingrese el nombre del producto: ");
+    let precio = await rl.question("Ingrese el precio del producto: ");
+
+    let producto = {
                 nombre: nombre,
                 precio: precio
             }
@@ -58,8 +73,8 @@ function agregarProducto() {
             console.log("\nProducto agregado correctamente.\n");
             menu();
 
-        });
-    });
+        
+    
 }
 
 //funcion para mostrar productos      
@@ -84,7 +99,7 @@ function mostrarProductos() {
 }
 
 //funcion para editar productos
-function editarProducto() {
+async function editarProducto() {
   
     if(productos.length === 0) {
         console.log("\nNo hay productos para editar.\n");
@@ -105,36 +120,83 @@ function editarProducto() {
         ); 
     }
 
-    rl.question("\nIngresa el número del producto que quieres editar:", function(numero) {
-    
-        let posicion = parseInt(numero) - 1;
+    let numero = await rl.question("\nIngresa el número del producto que quieres editar:");
+
+    let posicion = parseInt(numero) - 1;
 
         if (posicion >= 0 && posicion < productos.length){
             
-            rl.question("Ingrese el nuevo nombre del producto: ", function(nombre){
-
-                rl.question("Ingrese el nuevo precio del producto: ", function(precio){
+            let nombre = await rl.question("Ingrese el nuevo nombre del producto: ");
+            let precio = await rl.question("Ingrese el nuevo precio del producto: ");
 
                     productos[posicion].nombre = nombre;
                     productos[posicion].precio = parseFloat(precio);
 
                     console.log("\nProducto editado correctamente.\n");
                     menu();
-                    }
-                );
-            }
-        );
         
             }else{
             
                 console.log("\nNumero de producto invalido.\n");
                 menu();
             }
+    }    
+    
+
+
+async function buscarProducto(){
+    console.log("==== BUSCAR PRODUCTOS ====")
+    console.log("1. Productos caros")
+    console.log("2. Productos baratos")
+    console.log("3. Bebidas")
+    console.log("4. Postres")
+    console.log("5. Regresar al menu")
+    
+    let opcionn = await rl.question("Seleccione una opcion: ");
+    switch(opcionn){
+        case "1":
+            let productosCaros = productos.filter(producto => producto.precio > 20);
+            console.log("Productos caros:");
+            productosCaros.forEach(producto => {
+                    console.log(producto.nombre + " - $" + producto.precio);
+                }); 
+                buscarProducto();
+                break;
+            case "2":
+                let productosBaratos = productos.filter(producto => producto.precio <= 20);
+                console.log("Productos baratos:");
+                productosBaratos.forEach(producto => {
+                    console.log(producto.nombre + " - $" + producto.precio);
+                });
+                buscarProducto();
+                break;
+            case "3":
+                let bebidas = productos.filter(producto => producto.categoria === "Bebida");
+                console.log("Bebidas:");
+                bebidas.forEach(producto => {
+                    console.log(producto.nombre + " - $" + producto.precio);
+                });
+                buscarProducto();
+                break;
+            case "4":
+                let postres = productos.filter(producto => producto.categoria === "Postre");
+                console.log("Postres:");
+                postres.forEach(producto => {
+                    console.log(producto.nombre + " - $" + producto.precio);
+                });
+                buscarProducto();
+                break;
+            case "5":
+                menu();
+                break;
+            default:
+                console.log("Opcion invalida")
         }
-    );
+    
+    
 }
     
-function eliminarProducto() {
+async function eliminarProducto() {
 
     if(productos.length === 0) {
 
@@ -157,7 +219,7 @@ function eliminarProducto() {
         ); 
     }
 
-    rl.question("\nIngresa el número del producto que quieres eliminar:", function(numero){
+    let numero = await rl.question("\nIngresa el número del producto que quieres eliminar:");
 
         let posicion = parseInt(numero) - 1;
 
@@ -171,7 +233,7 @@ function eliminarProducto() {
 
     menu();
 
-    });
+    
 }
 
 //Inicia el menu
